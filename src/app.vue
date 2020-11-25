3b2d8c83cf245f07f8198574d8417b6b836bd289
MJoshua25 <46876900+MJoshua25@users.noreply.github.com>
2020-11-25 13:54:04 +0000

    Create app.vue

---
+++ src/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            storedRessources: [
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official Vue.js documentation',
+                    link: ''
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
